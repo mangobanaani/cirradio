5a58822add8cb6bb5d0fc044fc199598e1b85e6f
mangobanaani <psilt@iki.fi>
2026-03-16 22:27:22 +0200

    extend IAxiRegs with TRANSEC methods; implement in NullAxiRegs

--- software/src/security/IAxiRegs.h
+++ software/src/security/IAxiRegs.h
@@ -13,6 +13,12 @@ public:
     virtual void zeroize_fhek() = 0;
     // Writes CTRL_CLOCK_HALT to kill FPGA clock enables.
     virtual void halt_fpga_clocks() = 0;
+
+    // TRANSEC control
+    virtual void     set_emcon_ctrl(uint32_t value) = 0;
+    virtual void     write_emcon_unlock() = 0;
+    virtual void     set_tx_power(int32_t dBm_x100) = 0;
+    virtual uint32_t emcon_level() const = 0;
 };
 
 // No-op stub for tests on macOS / non-hardware builds.
@@ -20,6 +26,10 @@ class NullAxiRegs : public IAxiRegs {
 public:
     void zeroize_fhek() override {}
     void halt_fpga_clocks() override {}
+    void     set_emcon_ctrl(uint32_t) override {}
+    void     write_emcon_unlock() override {}
+    void     set_tx_power(int32_t) override {}
+    uint32_t emcon_level() const override { return 2; }
 };
 
 }  // namespace cirradio::security
